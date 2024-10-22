d567ba91e06094bb92b161bc1fa54ac1084cf0c0
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-10-22 18:31:08 +0200

    step 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
-/**
-* Principal - Point d'entrée du programme
-*
-* Ce programme imprime "La programmation, c'est comme construire un puzzle multilingue"
-* En utilisant la fonction puts, suivie d'une nouvelle ligne.
-*
-* Retour : Toujours 0 (Succés)
-*/
+
+/** je teste */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
