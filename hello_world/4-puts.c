be9e0508460f6d77a492825e5f2fad15dadadc7b
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-10-22 18:53:12 +0200

    step 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,13 +4,13 @@
  * main - Entry point of the program
  *
  * Description: This program prints "Programming is like building a multilingual
- * puzzle" u new line.
+ * puzzle" using the puts function, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual "
-"puzzle");
+puts("\"Programming is like building a multilingual puzzle\"");
 return (0);
 }
+
