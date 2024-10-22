a6e11b9e6096ee1d239749383ce4b8a4ce89907e
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-10-22 19:06:21 +0200

    step 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,8 +3,8 @@
 /**
  * main - Entry point of the program
  *
- * Description: This program prints "Programming is like building a multilingual
- * puzzle" using the puts function, followed by a new line.
+ * Description: Tram prints "Programming is like building a multilingual
+ * puzzle" using th, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
@@ -13,4 +13,3 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle\"");
 return (0);
 }
-
