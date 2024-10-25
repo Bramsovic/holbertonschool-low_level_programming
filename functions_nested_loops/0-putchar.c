9a091fa955ea43d372708970eb5a36713c51227c
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-10-25 14:04:49 +0200

    step 1

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
- * main - Entry point
+ * main - Entry point of the program
+ *
+ * This program prints "_putchar" followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("_putchar\n");
-return (0);
+const char *str = "_putchar"; /* Chaîne à afficher */
+/* Parcours de chaque caractère de la chaîne et affichage */
+while (*str)
+{
+_putchar(*str); /* Utiliser _putchar pour afficher le caractère */
+str++;          /* Passer au caractère suivant */
+}
+_putchar('\n'); /* Afficher un saut de ligne */
+return (0); /* Renvoie le succ�s*/
 }
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* Déclaration de la fonction _putchar */
+int _putchar(char c);
+#endif /* MAIN_H */
