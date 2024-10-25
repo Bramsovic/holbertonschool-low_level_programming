08fa0cc8a9dff8ab94763ac6517701527a128d92
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-10-25 14:53:20 +0200

    step 2

---
+++ functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Entry point to test the print_alphabet_x10 function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
+}
