71e89102523c8a7cc65d2b0f254b7e14b65dbe6a
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-10-30 22:30:30 +0100

    step 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - Affiche un caractère sur deux d'une chaîne de caractères,
+ * en commençant par le premier caractère.
+ * @str: pointeur vers la chaîne de caractères.
+ */
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i += 2;
+}
+_putchar('\n');
+}
