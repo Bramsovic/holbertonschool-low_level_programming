ed917ea237be708b928588f5127c48390f465622
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-10-30 23:39:39 +0100

    step 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copie une chaîne de caractères de src vers dest.
+ * @dest: pointeur vers le buffer de destination.
+ * @src: pointeur vers la chaîne source.
+ *
+ * Return: le pointeur vers dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return dest;  // Retourne le pointeur vers dest
+}
