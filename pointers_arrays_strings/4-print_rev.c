100c3c2a34924d82e913f437fbfe280f5191dd2e
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-10-30 22:09:03 +0100

    step 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - Affiche une chaîne de caractères en sens inverse suivie d'un saut de ligne.
+ * @s: pointeur vers la chaîne de caractères à afficher.
+ */
+void print_rev(char *s)
+{
+int length = 0;
+int i;
+while (s[length] != '\0')
+{
+length++;
+}
+for (i = length - 1; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
