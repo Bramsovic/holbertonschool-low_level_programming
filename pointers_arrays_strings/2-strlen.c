6fad38e800ff9dee267d81b95c1c96ee06d075e9
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-10-30 20:29:50 +0100

    step 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Retourne la longueur d'une chaîne de caractères.
+ * @s: pointeur vers la chaîne de caractères.
+ *
+ * Return: longueur de la chaîne.
+ */
+int _strlen(char *s)
+{
+int length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+return length;
+}
