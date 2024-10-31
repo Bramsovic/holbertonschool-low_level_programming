d2165ef4ed2999fbf7e8ccea418fc4c03bb3f9fc
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-10-31 21:48:24 +0100

    step 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,28 +8,24 @@
  */
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    int result = 0;
-    int started = 0;
-
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-            sign *= -1;
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            /* Check for potential overflow */
-            if (result > (2147483647 - (s[i] - '0')) / 10)
-                return (sign == 1 ? 2147483647 : -2147483648);
-
-            result = result * 10 + (s[i] - '0');
-            started = 1;
-        }
-        else if (started)
-            break;
-        i++;
-    }
-
-    return (sign * result);
+int i = 0;
+int sign = 1;
+int result = 0;
+int started = 0;
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+sign *= -1;
+else if (s[i] >= '0' && s[i] <= '9')
+{
+if (result > (2147483647 - (s[i] - '0')) / 10)
+return (sign == 1 ? 2147483647 : -2147483648);
+result = result * 10 + (s[i] - '0');
+started = 1;
+}
+else if (started)
+break;
+i++;
+}
+return (sign * result);
 }
