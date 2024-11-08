bc46d4360468704d746fac939f0066e4dd02b383
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-11-08 16:41:52 +0100

    step 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * _sqrt_recursion - returns the natural 
  * @n: the number to find the square root of
  * 
- * Return: the square root of n, or -1 if n does not have a natural square root
+ * Return: the square root of n
  */
 int _sqrt_helper(int n, int i);
 int _sqrt_recursion(int n)
