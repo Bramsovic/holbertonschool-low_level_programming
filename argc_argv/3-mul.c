e71d0a624ec347fb39a9a17845cba9407f337285
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-11-08 18:30:18 +0100

    step 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Entry point, multiplies two numbers
- * @argc: Argument count
- * @argv: Array of argument strings
+ * main - multiplies two numbers.
+ * @argc: argument count.
+ * @argv: argument vector.
  *
- * Return: 0 if successful
+ * Return: 0.
  */
 int main(int argc, char *argv[])
 {
-int num1, num2, result;
-if (argc != 3)
+int prod;
+if (argc == 3)
 {
+prod = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", prod);
+return (0);
+}
 printf("Error\n");
 return (1);
-}
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-result = num1 * num2;
-printf("%d\n", result);
-return (0);
 }
