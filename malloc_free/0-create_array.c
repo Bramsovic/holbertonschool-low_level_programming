9b2b2abcfd3ae3a1f3934658fc8eb73245e44d14
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-11-12 21:15:41 +0100

    step 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 
 /**
- * create_array - crée un tableau de caractères et l'initialise avec un char donné
- * @size: la taille du tableau
- * @c: le caractère à utiliser pour l'initialisation
+ * create_array - crée un tableau de caractères et l'initialise avec un char
+ * @size: taille du tableau
+ * @c: caractère d'initialisation
  *
- * Return: pointeur vers le tableau ou NULL en cas d'échec
+ * Return: pointeur vers le tableau ou NULL si échec ou taille zéro
  */
 char *create_array(unsigned int size, char c)
 {
