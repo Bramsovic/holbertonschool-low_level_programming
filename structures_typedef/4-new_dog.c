153a05617d0c8c6354d331503f4d96e225b9cabc
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-11-15 23:16:49 +0100

    step 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,7 +8,7 @@
  * @age: Âge du chien
  * @owner: Propriétaire du chien
  *
- * Return: Pointeur >chien, ou NULL >erreur
+ * Return: Pointeur > new chien, ou NULL >d'erreur
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -18,14 +18,14 @@ return (NULL);
 new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 return (NULL);
-new_dog->name = malloc(strlen(name) + 1);
+new_dog->name = malloc(sizeof(char) * (strlen(name) + 1));
 if (new_dog->name == NULL)
 {
 free(new_dog);
 return (NULL);
 }
 strcpy(new_dog->name, name);
-new_dog->owner = malloc(strlen(owner) + 1);
+new_dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 if (new_dog->owner == NULL)
 {
 free(new_dog->name);
