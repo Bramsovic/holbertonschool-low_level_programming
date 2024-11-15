43d7053d540805169be700b685e0bfcc9720df9f
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-11-15 21:07:23 +0100

    step 1

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - informations de base sur un chien
+ * @name: Nom du chien
+ * @age: Âge du chien
+ * @owner: Propriétaire du chien
+ *
+ * Description: Une structure définissant des informations de base sur un chien.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif /* DOG_H */
