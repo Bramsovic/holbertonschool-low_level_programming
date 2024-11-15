9f83303d16f953d43b4a3421565f4af021c20353
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-11-15 15:01:58 +0100

    step 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -6,7 +6,7 @@
  * @size: The size of each element in bytes.
  *
  * Return: Pointer to the allocated memory, or NULL if nmemb or size is 0,
- *         or if malloc fails.
+ * or if malloc fails.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
@@ -14,10 +14,10 @@ char *ptr;
 unsigned int i;
 if (nmemb == 0 || size == 0)
 return (NULL);
-ptr = malloc(nmemb * size);  // Allocation de la mémoire
+ptr = malloc(nmemb * size);
 if (ptr == NULL)
 return (NULL);
-for (i = 0; i < (nmemb * size); i++)  // Initialisation à zéro
+for (i = 0; i < (nmemb * size); i++)
 ptr[i] = 0;
 return (ptr);
 }
