f98863bde0f9bdafe11ff2287fd32de43fbca5f7
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-11-23 11:36:11 +0100

    step 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,6 +3,9 @@
  * print_name - focntion qui appelle fonction
  * @name: Pointeur
  * @f: Pointeur sur une fonction qui prend un pointeur
+ *
+ *Focntion qui utlise focntion
+ *if verifie que c'est pas nul
  */
 void print_name(char *name, void (*f)(char *))
 {
