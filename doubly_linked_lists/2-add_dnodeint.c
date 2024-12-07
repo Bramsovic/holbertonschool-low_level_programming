fe15ceecd1eef809f00ec02a5920f6de1597c544
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-12-07 14:49:54 +0100

    fix error

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -5,8 +5,8 @@
 /**
  * add_dnodeint - Ajoute un nouveau noeud au début de la liste
  * @head: Le pointer du premier noeud de la liste
- *
  * Return: La nouvelle liste, ou NULL si cela échoue.
+ * @n: l'entier stocké dans le noeud à remplacer par l'entier en entrée.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
@@ -14,22 +14,11 @@ dlistint_t *debutliste;
 debutliste = malloc(sizeof(dlistint_t));
 if (debutliste == NULL)
 return (NULL);
- debutliste->n = n;
-if (debutliste->n == 0)
-{
-free(debutliste);
-return (0);
-}
-if(*head == NULL)
-{
-debutliste->next = NULL;
-*head = debutliste;
-}
-else
-{
+debutliste->n = n;
 debutliste->next = *head;
+debutliste->prev = NULL;
+if (*head != NULL)
 (*head)->prev = debutliste;
 *head = debutliste;
-}
 return (debutliste);
 }
