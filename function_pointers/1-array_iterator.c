8f8cb1cfdb52fa63e0b2143f1945b92106779e99
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-12-14 15:33:22 +0100

    fix error betty

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 /**
  * array_iterator - appel de fonction
- * @array: tableau 
+ * @array: tableau
  * @size: la taille du tableau 5
  * @action: un pointeur de fonction à appliquer sur chaque élément
  *
@@ -13,6 +13,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 unsigned int i;
 if (action == NULL || array == NULL)
 return;
-for ( i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 (action)(array[i]);
 }
