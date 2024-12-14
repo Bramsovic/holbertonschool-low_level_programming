b6ee9aac83d9d9ade84e1438c4bcb9348643aef8
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-12-14 15:27:30 +0100

    Duplique une chaîne de caractères

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,14 +1,45 @@
-#include "dog.h"
 #include <stdlib.h>
-#include <string.h>
-
+#include "dog.h"
 /**
- * new_dog - Creates a new dog.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
+ * _strdup - Duplique une chaîne de caractères
+ * @str: Chaîne à dupliquer
  *
- * Return: Pointer to the new dog, or NULL if failure.
+ * Return: Pointeur vers la nouvelle chaîne, ou NULL si échec
  */
-
-dog_t *new_dog(char *name, float age, char *owner);
+char *_strdup(char *str)
+{
+char *dup;
+int i, len = 0;
+if (str == NULL)
+return (NULL);
+while (str[len])
+len++;
+dup = malloc(sizeof(char) * (len + 1));
+if (dup == NULL)
+return (NULL);
+for (i = 0; i <= len; i++)
+dup[i] = str[i];
+return (dup);
+}
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *dog;
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+dog->name = _strdup(name);
+if (dog->name == NULL)
+{
+free(dog);
+return (NULL);
+}
+dog->owner = _strdup(owner);
+if (dog->owner == NULL)
+{
+free(dog->name);
+free(dog);
+return (NULL);
+}
+dog->age = age;
+return (dog);
+}
