42ec2847ca1a6b87a5457c3dca7e4c0757f9b4f1
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-12-14 23:55:16 +0100

    Prints anything based on the given formatV2

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -12,38 +12,43 @@
  */
 void print_all(const char * const format, ...)
 {
-va_list arguments;
-unsigned int format_index = 0;
-char *separator_string = "";
-char *string_argument;
-va_start(arguments, format);
-while (format && format[format_index])
-{
-if (format[format_index] == 'c')
-{
-printf("%s%c", separator_string, va_arg(arguments, int));
-separator_string = ", ";
-}
-else if (format[format_index] == 'i')
-{
-printf("%s%d", separator_string, va_arg(arguments, int));
-separator_string = ", ";
-}
-else if (format[format_index] == 'f')
-{
-printf("%s%f", separator_string, va_arg(arguments, double));
-separator_string = ", ";
-}
-else if (format[format_index] == 's')
-{
-string_argument = va_arg(arguments, char *);
-if (!string_argument)
-string_argument = "(nil)";
-printf("%s%s", separator_string, string_argument);
-separator_string = ", ";
-}
-format_index++;
-}
-va_end(arguments);
-printf("\n");
+    va_list arguments;
+    unsigned int format_index = 0;
+    char *separator_string = "";
+    char *string_argument;
+
+    va_start(arguments, format);
+
+    while (format && format[format_index])
+    {
+        if (format[format_index] == 'c')
+        {
+            printf("%s%c", separator_string, va_arg(arguments, int));
+        }
+        else if (format[format_index] == 'i')
+        {
+            printf("%s%d", separator_string, va_arg(arguments, int));
+        }
+        else if (format[format_index] == 'f')
+        {
+            printf("%s%f", separator_string, va_arg(arguments, double));
+        }
+        else if (format[format_index] == 's')
+        {
+            string_argument = va_arg(arguments, char *);
+            if (!string_argument)
+                string_argument = "(nil)";
+            printf("%s%s", separator_string, string_argument);
+        }
+        else
+        {
+            format_index++;
+            continue;
+        }
+        separator_string = ", ";
+        format_index++;
+    }
+
+    va_end(arguments);
+    printf("\n");
 }
