cb0e9f149f6a1725dd5c8d8e1895256bcad1979b
« bramsovic» <haddad.brahim.59000@gmail.com>
2024-12-15 21:45:19 +0100

    Add simple calculator program
    - 3-calc.h: Contains structure and function prototypes.
    - 3-op_functions.c: Implements basic operations (add, sub, mul, div, mod).
    - 3-get_op_func.c: Selects the correct operation based on input.
    - 3-main.c: Handles input, validates arguments, and prints results."

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,18 +1,21 @@
-#ifndef CALC_H
-#define CALC_H
-#include <stddef.h> 
-/* Déclaration de Prototype*/
+#ifndef _CALC_H_
+#define _CALC_H_
 
-struct op
+/**
+ * struct op - Struct linking an operator to its function.
+ * @op: Operator as a string (e.g., "+", "-").
+ * @f: Pointer to the function performing the operation.
+ */
+typedef struct op
 {
 char *op;
-int (*f)(int a, int b); 
-};
-typedef struct op op_t;
-int (*get_op_func(char *s))(int, int);
+int (*f)(int a, int b);
+}
+op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 #endif
--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,24 +1,31 @@
-#include <stdlib.h>
-#include "3-calc.h"
+#include <stddef.h>
 #include <string.h>
+#include "3-calc.h"
+/**
+* get_op_func - pointer on operations functions
+* @s: string which contains the operator
+* Return: one integer calculated by operators or NULL.
+*/
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-unsigned int i = 0; 
-if (s == NULL)
-return NULL;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+
+	};
+	int i = 0;
+
 while (ops[i].op != NULL)
 {
 if (strcmp(ops[i].op, s) == 0)
+{
 return (ops[i].f);
+}
 i++;
 }
-return NULL;
+return (NULL);
 }
